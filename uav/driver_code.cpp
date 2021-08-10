20825691f493ac64b0c27ba27386fe1df3a0008b
acse-jpl20 <71903012+acse-jpl20@users.noreply.github.com>
2021-08-10 19:50:20 +0100

    Changes to drone clusters, also added all_distance and all_masses inputs

--- uav/driver_code.cpp
+++ uav/driver_code.cpp
@@ -46,47 +46,58 @@ void set_GA_params(GA_param_list& lst)
 	lst.tolerance = 1.0e-3;
 }
 int drone_clusters(int k, int part_size, vector<unique_ptr<address_metadata>>& cl_data, vector<address_metadata>& opt_route, double max_payload_cap,
-					double max_dist)
+					double max_dist, double& all_distances, double& all_masses)
 {
 	int cnt, cnt2, temp_num_of_addresses_distance = 0, temp_num_of_addresses_mass = 0;
 	double sum_d, sum_m;
+	all_distances = 0, all_mass = 0;
 	int number_of_addresses = 0;
 	vector<double> total_dist_v(part_size + 2); 
 	vector<double> cluster_mass_v(part_size + 1);
-	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < cl_data[i].size(); j++) {
+	for (int i = 0; i < k; i++) 
+	{ //Iterate over each cluster
+		for (int j = 0; j < cl_data[i].size();) 
+		{ //Iterate over all addresses with id i 
 			cnt = 0;
 			sum_m = 0;
 			sum_d = 0;
-			total_dist_v[cnt] =  utility::length(opt_route[i], cl_data[i][j]); // cool
-			for (int k = j; k < j + part_size; k++) {
-				total_dist_v[cnt] = utility::length(cl_data[i][k], cl_data[i][k + 1]);
+			total_dist_v[cnt] =  utility::length(opt_route[i], cl_data[i][j]); //Calculate distance from centroid(id = i) to address with matching id
+			for (int k = j; k < j + part_size; k++) 
+			{ //Iterate over user given part_size (potential maximum number of addresses a single drone can visit)
+				total_dist_v[cnt] = utility::length(cl_data[i][k], cl_data[i][k + 1]); //Calculate distance between addresses
 				cnt++;
 			}
-			total_dist_v[cnt] = utility::length(cl_data[i][part_size + j], opt_route[i]);
-			cnt = 0;
-			for (int k = j; k < j + part_size + 1; k++) {
-				cluster_mass_v[cnt] = cl_data[i][k + 1].parcel_mass;
+			total_dist_v[cnt] = utility::length(cl_data[i][part_size + j], opt_route[i]); //Calculate distance from last address to centroid with matching id
+			cnt = 0; //Assing cnt to 0 to reuse as iterating over parcel mass size now
+			for (int k = j; k < j + part_size + 1; k++) 
+			{
+				cluster_mass_v[cnt] = cl_data[i][k + 1].parcel_mass; //Parcel mass of each address
 				cnt++;
 			}
 			temp_num_of_addresses_distance = part_size + 1;
-			sum_d += std::accumulate(total_dist_v.begin(), total_dist_v.end(), 0.);
-			sum_m += std::accumulate(cluster_mass_v.begin(), cluster_mass_v.end(), 0.);
-			cnt = part_size;
-			while (sum_m > max_payload_cap) {
-				//auto it2 = max_element(cluster_mass_v.begin(), cluster_mass_v.end());
+			temp_num_of_addresses_mass = part_size + 1;
+			sum_d += std::accumulate(total_dist_v.begin(), total_dist_v.end(), 0.); //Calculate total distance 
+			sum_m += std::accumulate(cluster_mass_v.begin(), cluster_mass_v.end(), 0.); //Calculate total mass 
+			cnt = part_size; //Reusing cnt to iterate backwards
+			while (sum_m > max_payload_cap) 
+			{ // iterate backwards over mass first as do not need to recalculate distance
 				sum_m -= cluster_mass_v[cnt];
-				cnt--;  //*max_element(cluster_mass_v.begin(), cluster_mass_v.end());
 				temp_num_of_addresses_mass -= 1;
 			}
-			if (temp_num_of_addresses_mass != 1) {
+			cnt2 = j + part_size - 1;
+			if (temp_num_of_addresses_mass == 1)
+				sum_d = utility::length(cl_data[i][cnt2], opt_route[i]);
+	
+			else if (temp_num_of_addresses_mass != 0) 
+			{//If payload capacity for a single drone has been met no need to recalculate distances
 				cnt = part_size + 1;
 				while (sum_d > max_dist) {
 					sum_d -= total_dist_v[cnt];
 					cnt--;
 					sum_d -= total_dist_v[cnt];
-					sum_d += utility::length(cl_data[][], opt_route[i]);
 					cnt--;
+					sum_d += utility::length(cl_data[i][cnt2], opt_route[i]);
+					cnt2--;
 					temp_num_of_addresses_distance -= 1;
 
 				}
@@ -95,11 +106,15 @@ int drone_clusters(int k, int part_size, vector<unique_ptr<address_metadata>>& c
 			int lowest_num = min(temp_num_of_addresses_distance, temp_num_of_addresses_mass);
 			for (int k = j; k < lowest_num + j; k++) {
 				cl_data[i][k].visited = true;
+				
 			}
+			all_distances += sum_d;
+			all_mass += sum_m;
 			number_of_addresses += lowest_num;
 			j += lowest_num;
 		}
 	}
+	
 
 }
 double run_truck_tandem_drone(string drone_type)
@@ -116,7 +131,8 @@ double run_truck_tandem_drone(string drone_type)
 	genetic_algorithm<Circuit, address_metadata> vb(f, cl);
 	result Result = vb.run_algorithm_genetic(10, &fitness, &initialise_circuit_v, &check_truck_route_validity);
 	Result.circuit_vector;
-	int no_of_drones_req = drone_clusters(cl.k, 2, cl.cluster_regions, cl.centroids, temp_drone., temp_drone.);
+	double total_mass, total_distance;
+	int no_of_drones_req = drone_clusters(cl.k, 2, cl.cluster_regions, cl.centroids, temp_drone., temp_drone., total_distance, total_mass);
 	setup_drones(no_of_drones_req, "rural");
 	return total_cost_rural(no_of_drones_req, temp_drone., Result.optimal_performance, , );
 
